4f34b0e6f29084e6dec38f520f6bc262be2b9814
Maxime Oudin <oudinmaxime@gmail.com>
2025-10-30 22:40:57 +0100

    start translations

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|fr/ do
+    devise_for :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get "up" => "rails/health#show", as: :rails_health_check
 
-  root "pages#index"
-  resources :projects
-  resources :tech_stacks
+    root "pages#index"
+    resources :projects
+    resources :tech_stacks
+  end
 end
